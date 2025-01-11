db00b4fb10bd189f05ff8a03a6b082b478cb3ca7
Surenderdubeyofficial <surenderdubey221@gmail.com>
2025-01-11 14:20:31 +0530

    Add .env file and update server.js

--- server.js
+++ server.js
@@ -12,10 +12,13 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose
-    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('MongoDB connection error:', err));
+
+const mongoURI = process.env.MONGO_URI; // Get URI from environment variable
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
+
 
 // Factorial Logic
 function factorialIterative(n) {
